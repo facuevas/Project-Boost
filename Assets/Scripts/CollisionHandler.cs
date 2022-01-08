8701569407af92ff2aa114d1a40c40652bf72ace
Angelo <franciscuevas4@gmail.com>
2022-01-07 22:40:44 -0800

    Added sound files for death and success states

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,30 +3,45 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField]
+    private float m_loadLevelDelay = 1.5f;
+
     private void OnCollisionEnter(Collision other)
     {
         var otherTag = other.gameObject.tag;
         switch (otherTag)
         {
-            case "Fuel":
-                Debug.Log("Collided with a Fuel object");
-                break;
             case "Friendly":
                 Debug.Log("Collided with a Friendly object");
                 break;
             case "Finished":
-                LoadNextLevel();
+                LoadNextLevelSequence();
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
+    }
+
+    private void StartCrashSequence()
+    {
+        //TODO: Add SFX upon crash.
+        //TODO: Add particle effect upon crash.
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", m_loadLevelDelay);
     }
 
     private void ReloadLevel()
     {
         int currentActiveSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentActiveSceneIndex);
+    }
+    private void LoadNextLevelSequence()
+    {
+        //TODO: Add SFX upon crash.
+        //TODO: Add particle effect upon crash.
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", m_loadLevelDelay);
     }
 
     private void LoadNextLevel()
